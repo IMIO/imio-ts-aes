643ab07291d72c0f572bdb2ebb36e2a82e9a1c10
Christophe Boulanger <christophe.boulanger@imio.be>
2018-08-21 13:40:10 +0200

    update postinst script

--- install_imio-ts-aes.sh
+++ install_imio-ts-aes.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+
+# WCS : Create symbolic links where storing wcs webservices calling.
 for i in /var/lib/wcs/*/ ; do ln -s /usr/share/imio-ts-aes/wscalls $i/wscalls; done
+# WCS : Get wcs tenant
+wcs_tenant=$(python /usr/share/imio-ts-aes/get-wcs-tenant.py 2>&1)
+# WCS : Script to import xml workflow in wcs
+sudo -u  wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=$wcs_tenant /usr/share/imio-ts-aes/import-workflows.py
+# WCS : Script to import xml forms in wcs
+sudo -u  wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=$wcs_tenant /usr/share/imio-ts-aes/import-forms.py
