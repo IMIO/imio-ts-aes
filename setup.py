3dfe336c128047e99cb9cd05bc461dd236bb933d
Christophe Boulanger <christophe.boulanger@imio.be>
2019-01-22 08:58:50 +0100

    change setup.py

--- setup.py
+++ setup.py
@@ -1,88 +1,24 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import glob
-import os
-import re
-import subprocess
-import sys
-
-from setuptools.command.install_lib import install_lib as _install_lib
-from distutils.command.build import build as _build
-from distutils.command.sdist import sdist
-from distutils.cmd import Command
+from setuptools import setup
+from setuptools.command.install import install
 from setuptools import setup, find_packages
+import os
 
-class eo_sdist(sdist):
+class inst(install):
     def run(self):
-        if os.path.exists('VERSION'):
-            os.remove('VERSION')
-        version = get_version()
-        version_file = open('VERSION', 'w')
-        version_file.write(version)
-        version_file.close()
-        sdist.run(self)
-        if os.path.exists('VERSION'):
-            os.remove('VERSION')
-
-def get_version():
-    if os.path.exists('VERSION'):
-        version_file = open('VERSION', 'r')
-        version = version_file.read()
-        version_file.close()
-        return version
-    if os.path.exists('.git'):
-        p = subprocess.Popen(['git', 'describe', '--dirty', '--match=v*'], stdout=subprocess.PIPE)
-        result = p.communicate()[0]
-        if p.returncode == 0:
-            version = result.split()[0][1:]
-            version = version.replace('-', '.')
-            return version
-    return '0'
-
-
-class compile_translations(Command):
-    description = 'compile message catalogs to MO files via django compilemessages'
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            from django.core.management import call_command
-            for path, dirs, files in os.walk('imio_ts_aes'):
-                if 'locale' not in dirs:
-                    continue
-                curdir = os.getcwd()
-                os.chdir(os.path.realpath(path))
-                call_command('compilemessages')
-                os.chdir(curdir)
-        except ImportError:
-            sys.stderr.write('!!! Please install Django >= 1.4 to build translations\n')
-
-
-class build(_build):
-    sub_commands = [('compile_translations', None)] + _build.sub_commands
-
-
-class install_lib(_install_lib):
-    def run(self):
-        self.run_command('compile_translations')
-        _install_lib.run(self)
-
+        install.run(self)
+        path = os.getcwd().replace(" ", "\ ").replace("(","\(").replace(")","\)") + "/bin/"
+        os.system("sh "+path+"install_imio-ts-aes.sh")
 
 setup(
     name='imio-ts-aes',
-    version=get_version(),
     author='Christophe Boulanger',
     author_email='christophe.boulanger@imio.be',
     packages=find_packages(),
     include_package_data=True,
-    url='https://dev.entrouvert.org/projects/imio/',
+    url='https://github.com/IMIO/imio-ts-aes',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Environment :: Web Environment',
@@ -95,9 +31,6 @@ setup(
     ],
     zip_safe=False,
     cmdclass={
-        'build': build,
-        'compile_translations': compile_translations,
-        'install_lib': install_lib,
-        'sdist': eo_sdist,
+        'inst': inst,
     }
 )
