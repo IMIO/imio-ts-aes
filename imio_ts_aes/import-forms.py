995c67e1c149f3a46e3fbe391371430ae757027d
Christophe Boulanger <christophe.boulanger@imio.be>
2020-02-03 14:15:53 +0100

    blackened file

--- imio_ts_aes/import-forms.py
+++ imio_ts_aes/import-forms.py
@@ -1,19 +1,21 @@
 import os
 import sys
+
 from wcs.formdef import FormDef
+
 lst_formdef_ids = []
 lst_formdef_names = []
 conv = lambda x: int(x)
 for formdef in FormDef.select():
     lst_formdef_names.append(formdef.name)
     lst_formdef_ids.append(formdef.id)
-folder_path = '{0}/forms/'.format(sys.argv[1])
+folder_path = "{0}/forms/".format(sys.argv[1])
 for fichier in os.listdir(folder_path):
     if fichier[-4:] == ".wcs":
         fd = open("{}{}".format(folder_path, fichier))
         try:
-            formdef = FormDef.import_from_xml(fd, charset='utf-8', include_id=False)
-        except:
+            formdef = FormDef.import_from_xml(fd, charset="utf-8", include_id=False)
+        except Exception:
             print "!!! ECHEC IMPORT FILE : {}".format(fichier)
             formdef = None
         if formdef is not None:
@@ -27,6 +29,5 @@ for fichier in os.listdir(folder_path):
                     lst_formdef_ids.append(new_id)
                     formdef.id = new_id
                     formdef.store()
-                except:
+                except Exception:
                     print "import error : {}".format(formdef.id)
-
