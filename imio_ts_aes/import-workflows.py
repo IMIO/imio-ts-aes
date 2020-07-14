3a2c00b0b453f3d2a2079f30d17b57c579fc6264
Christophe Boulanger <christophe.boulanger@imio.be>
2020-07-14 09:25:19 +0200

    Create script to add extra roles. Add traces in installation script.

--- imio_ts_aes/import-workflows.py
+++ imio_ts_aes/import-workflows.py
@@ -5,6 +5,7 @@ from wcs.workflows import Workflow
 folder_path = '{0}/workflows/'.format(sys.argv[1])
 for fichier in os.listdir(folder_path):
     if fichier[-4:] == ".wcs":
+        print(fichier)
         fd = open("{}{}".format(folder_path, fichier))
         wf = Workflow.import_from_xml(fd)
         wf.store()
--- imio_ts_aes/install_imio-ts-aes.sh
+++ imio_ts_aes/install_imio-ts-aes.sh
@@ -7,14 +7,14 @@ USER="wcs"
 
 domain=$1
 
-# installation path
+echo "--- installation path"
 install_path="/usr/lib/imio_ts_aes"
 
-# WCS : Get wcs tenant
-wcs_tenant=$(python3 $install_path/get-wcs-tenant.py 2>$1)
+echo "--- WCS : Get wcs tenant"
+wcs_tenant=$(python3 $install_path/get-wcs-tenant.py 2>&1)
 commune=$(echo "$wcs_tenant" | sed "s/-formulaires.$domain//")
 
-# COMBO : Get combo tenant
+echo "--- COMBO : Get combo tenant"
 combo_tenant=$(echo "$wcs_tenant" | sed "s/-formulaires//")
 
 if ! [ -d /var/lib/wcs/$wcs_tenant/wscalls/ ]
@@ -23,7 +23,7 @@ then
 fi
 cp $install_path/wscalls/* /var/lib/wcs/$wcs_tenant/wscalls
 
-# WCS : Create categories (Categories must be create before forms, category can't be an alphanumeric name...)
+echo "--- WCS : Create categories (Categories must be create before forms, category can't be an alphanumeric name...)"
 if [ -z `grep accueil-extra-scolaire /var/lib/wcs/*/categories -nri` ]; then
     echo 'install category'
     category_registration_number=$(ls /var/lib/wcs/$wcs_tenant/categories |  sort -n | tail -1)
@@ -35,7 +35,7 @@ else
     echo 'category already exist'
 fi
 
-# Deploy passerelle plugins.
+echo "--- Deploy passerelle plugins."
 sudo -u passerelle /usr/bin/passerelle-manage tenant_command import_site -d $commune-passerelle.$domain $install_path/passerelle/iimioiaaes.json
 
 if [ -z `grep demo_aes_repas /var/lib/wcs/$wcs_tenant/datasources -nri` ]; then
@@ -49,23 +49,26 @@ else
     echo 'datasources already exists'
 fi
 
-chown -R ${USER}:${USER} /var/lib/wcs/$wcs_tenant/wscalls/aes* 
-# chown -R ${USER}:${USER} $install_path/datasources 
+chown -R ${USER}:${USER} /var/lib/wcs/$wcs_tenant/wscalls/aes*
+# chown -R ${USER}:${USER} $install_path/datasources
 
-# WCS : Script to import xml workflow in wcs (Workflows must be create before forms)
+echo "--- Create roles"
+authentic2-multitenant-manage tenant_command runscript create-roles.py -d $commune-auth.$domain
+
+echo "--- WCS : Script to import xml workflow in wcs (Workflows must be create before forms)"
 sudo -u  wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=$wcs_tenant $install_path/import-workflows.py $install_path
 
-# WCS : Script to import xml forms in wcs
+echo "---  WCS : Script to import xml forms in wcs"
 sed -i -E 's|category_id=.*>(.*)</category>|category_id="'$(($category_registration_number + 1))'">\1</category>|' $install_path/forms/*.wcs
 sudo -u  wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=$wcs_tenant $install_path/import-forms.py $install_path
 
-# COMBO : import AES widget (settings.json)
+echo "---  COMBO : import AES widget (settings.json)"
 sed -i "s/[COMMUNE]/$commune/g" $install_path/combo/tenants/settings.json
 sed -i "s/[DOMAIN]/$domain/g" $install_path/combo/tenants/settings.json
 cp $install_path/combo/tenants/settings.json /var/lib/combo/tenants/$combo_tenant/
 sed -i "s/$commune/[COMMUNE]/g" $install_path/combo/tenants/settings.json
 sed -i "s/$domain/[DOMAIN]/g" $install_path/combo/tenants/settings.json
 
-# COMBO : import AES portail-citoyen structure
+echo "--- COMBO : import AES portail-citoyen structure"
 sudo -u combo combo-manage tenant_command import_site -d $combo_tenant $install_path/combo/portail-citoyen-aes.json
 sudo -u combo combo-manage tenant_command import_site -d $combo_tenant $install_path/combo/portail-agent-aes.json
