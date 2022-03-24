f4f67b31467bf35dab2ff29b407aa16815a4aa5c
Nicolas Hislaire <nicolas.hislaire@imio.be>
2022-03-24 16:45:51 +0100

    [MPP-181] add homepage cell config

--- imio_ts_aes/combo/tenants/settings.py
+++ imio_ts_aes/combo/tenants/settings.py
@@ -1,34 +1,8 @@
 JSON_CELL_TYPES.update(
     {
-        "wcs-aes-auth": {
-            "name": "AES - Formulaires disponibles",
-            "url": "{{passerelle_url}}passerelle-imio-ia-aes/aes/is_registered_parent?email={{ user_email }}",
-            "additional-data": [
-                {
-                    "key": "aes_forms",
-                    "url": "{{eservices_url}}api/categories/portail-parent/formdefs/",
-                },
-                {
-                    "key": "aes_children",
-                    "url": "{{passerelle_url}}passerelle-imio-ia-aes/aes/get_children_by_parent_nrn?email={{ user_email }}&nrn={{ request.session.mellon_session.niss.0 }}",
-                },
-            ],
-        },
-        "aes-known-children": {
-            "name": "AES - Enfants connus dans votre guichet",
-            "url": "{{ passerelle_url }}passerelle-imio-ia-aes/aes/get_children_by_parent_nrn?email={{ user_email }}&nrn={{ request.session.mellon_session.niss.0 }}",
-        },
-        "aes-plaines": {
-            "name": "AES - Semaines de plaines",
-            "url": "{{ eservices_url}}api/user/forms?email={{ user_email }}&full=on",
-        },
-        "aes-info-plaines": {
-            "name": "AES - Informations Plaines",
-            "url": "{{hobo_url}}hobos.json",
-        },
-        "aes-info-repas": {
-            "name": "AES - Informations Repas",
-            "url": "{{hobo_url}}hobos.json",
+        "portail_parent": {
+            "name": "Portail Parent - Page d'accueil",
+            "url": "{{ passerelle_url }}passerelle-imio-ia-aes/aes/parents/{{ request.session.mellon_session.aes_id.0 }}/homepage/",
         },
     }
 )
