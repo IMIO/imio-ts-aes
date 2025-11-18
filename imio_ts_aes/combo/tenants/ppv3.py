9e74020c3320687f131191701be20e7ecf654842
Nicolas Hislaire <nicolas.hislaire@imio.be>
2025-11-18 12:35:17 +0100

    [PP-602] Set PPv3 combo's cells config

---
+++ imio_ts_aes/combo/tenants/ppv3.py
@@ -0,0 +1,165 @@
+JSON_CELL_TYPES.update(
+   {
+      "pp_activites": {
+         "name": "Portail Parent - Activités",
+         "url": "{{ idp_url }}api/users/{{ user_nameid }}",
+         "cache_duration": 0,
+         "additional-data": [
+            {
+               "key": "formulaires",
+               "url": "{{ passerelle_url }}passerelle-imio-ia-aes/aes/get-forms?keywords=inscriptions,désinscriptions"
+            },
+            {
+               "key": "children",
+               "url": "{{ passerelle_url }}passerelle-imio-ia-aes/aes/parents/{{ json.aes_id|default:'null' }}/children/"
+            },
+            {
+               "key": "check_invoiceable_disponibility",
+               "url": "{{ eservices_url }}api/formdefs/pp-me-designer-facturable/schema"
+            },
+            {
+               "key": "homepage_data",
+               "url": "{% if json.aes_id %}{{ passerelle_url }}passerelle-imio-ia-aes/aes/parents/{{ json.aes_id }}/homepage?parent_uuid={{ user_nameid }}{% endif %}"
+            }
+
+         ]
+      }
+   }
+)
+
+JSON_CELL_TYPES.update(
+   {
+      "pp_alimenter_portefeuille": {
+         "name": "Portail Parent - Solde portefeuille par catégorie",
+         "url": "{{ idp_url }}api/users/{{ user_nameid }}",
+         "cache_duration": 0,
+         "additional-data": [
+            {
+               "key": "balances_data",
+               "url": "{% if json.aes_id %}{{ passerelle_url }}passerelle-imio-ia-aes/aes/parents/{{ json.aes_id }}/balances{% endif %}"
+            }
+         ]
+      }
+   }
+)
+
+
+JSON_CELL_TYPES.update(
+   {
+      "pp_attestations": {
+         "name": "Portail Parent - Attestations",
+         "url": "{{ idp_url }}api/users/{{ user_nameid }}",
+         "cache_duration": 0,
+         "force_async": True,
+         "additional-data": [
+            {
+               "key": "data",
+               "url": "{{ passerelle_url }}passerelle-imio-ia-aes/aes/parents/{{ json.aes_id }}/certificates/"
+            }
+         ]
+      }
+   }
+)
+
+JSON_CELL_TYPES.update(
+   {
+      "pp_bienvenue_portail_parent": {
+         "name": "Portail Parent - Bienvenue sur le Portail Parent",
+         "cache_duration": 0,
+         "url": "w"
+      }
+   }
+)
+JSON_CELL_TYPES.update(
+    {
+        "pp_bienvenue_portail_parent_documents": {
+            "name": "Portail Parent - Documents à télécharger",
+            "cache_duration": 0,
+            "url": "w",
+            "form": [
+                {"varname": "doc1_nom", "type": "string", "label": "Nom du document 1"},
+                {"varname": "doc1_url", "type": "string", "label": "URL du document 1"},
+                {"varname": "doc2_nom", "type": "string", "label": "Nom du document 2"},
+                {"varname": "doc2_url", "type": "string", "label": "URL du document 2"},
+                {"varname": "doc3_nom", "type": "string", "label": "Nom du document 3"},
+                {"varname": "doc3_url", "type": "string", "label": "URL du document 3"},
+                {"varname": "doc4_nom", "type": "string", "label": "Nom du document 4"},
+                {"varname": "doc4_url", "type": "string", "label": "URL du document 4"},
+                {"varname": "doc5_nom", "type": "string", "label": "Nom du document 5"},
+                {"varname": "doc5_url", "type": "string", "label": "URL du document 5"},
+                {"varname": "doc6_nom", "type": "string", "label": "Nom du document 6"},
+                {"varname": "doc6_url", "type": "string", "label": "URL du document 6"},
+            ]
+        }
+    }
+)
+
+JSON_CELL_TYPES.update(
+   {
+      "pp_enfants": {
+         "name": "Portail Parent - Enfants",
+         "url": "{{ idp_url }}api/users/{{ user_nameid }}",
+         "cache_duration": 0,
+         "force_async": True,
+         "additional-data": [
+            {
+               "key": "homepage_data",
+               "url": "{% if json.aes_id %}{{ passerelle_url }}passerelle-imio-ia-aes/aes/parents/{{ json.aes_id }}/homepage?parent_uuid={{ user_nameid }}{% endif %}"
+            }
+         ]
+      }
+   }
+)
+
+JSON_CELL_TYPES.update(
+   {
+      "pp_factures": {
+         "name": "Portail Parent - Factures",
+         "url": "{{ idp_url }}api/users/{{ user_nameid }}",
+         "cache_duration": 0,
+         "force_async": True,
+         "additional-data": [
+            {
+               "key": "data",
+               "url": "{{ passerelle_url }}passerelle-imio-ia-aes/aes/parents/{{ json.aes_id }}/invoices/"
+            }
+         ]
+      }
+   }
+)
+
+JSON_CELL_TYPES.update(
+   {
+      "pp_general_factures_attestations": {
+         "name": "Portail Parent - Général Factures et Attestations",
+         "cache_duration": 0,
+         "url": "w"
+      }
+   }
+)
+
+JSON_CELL_TYPES.update(
+   {
+      "pp_inscriptions": {
+         "name": "Portail Parent - Inscriptions aux plaines et stages",
+         "url": "{{ idp_url }}api/users/{{ user_nameid }}",
+         "cache_duration": 0,
+         "force_async": True,
+         "additional-data": [
+            {
+               "key": "homepage_data",
+               "url": "{% if json.aes_id %}{{ passerelle_url }}passerelle-imio-ia-aes/aes/parents/{{ json.aes_id }}/homepage_lite{% endif %}"
+            },
+            {
+                "key": "formulaires",
+                "url": "{{ passerelle_url }}passerelle-imio-ia-aes/aes/get-forms?keywords=fiche santé,plaines de vacances"
+            },
+            {
+                "key": "check_invoiceable_disponibility",
+                "url": "{{ eservices_url }}api/formdefs/pp-me-designer-facturable/schema"
+            }
+
+         ]
+      }
+   }
+)
--- imio_ts_aes/install_imio-ts-aes.sh
+++ imio_ts_aes/install_imio-ts-aes.sh
@@ -11,11 +11,8 @@ then
   service passerelle restart
 fi
 
-# if [ ! -f "/etc/combo/settings.d/aes.py" ]
-# then
-cp $install_path/combo/tenants/settings.py /etc/combo/settings.d/aes.py
-service combo restart
-# fi
+echo "--- Install combo settings for parent portal's custom cells"
+ln -s $install_path/combo/tenants/ppv3.py /etc/combo/settings.d/ppv3.py --force 
 
-
+echo "--- Deploy parent portal"
 sudo -u hobo hobo-manage imio_indus_deploy --directory $install_path
